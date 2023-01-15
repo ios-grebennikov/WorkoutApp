fe25a812ab8b8af088c130e3e2c734dfba208a3c
Alexey Grebennikov <ios.grebennikov@gmail.com>
2023-01-15 12:46:03 +0300

    ADD: - Created coordinate grid at MonthlyPerfomanceView

---
+++ WorkoutApp/WorkoutApp/Controllers/Base/Views/WAChartsView/Views/WAChartView.swift
@@ -0,0 +1,89 @@
+//
+//  WAChartView.swift
+//  WorkoutApp
+//
+//  Created by Alexey Grebennikov on 15.01.23.
+//
+
+import UIKit
+
+final class WAChartView: WABaseView {
+    
+    private let yAxisSeparator: UIView = {
+        let view = UIView()
+        view.backgroundColor = R.Colors.separator
+        return view
+    }()
+    
+    private let xAxisSeparator: UIView = {
+        let view = UIView()
+        view.backgroundColor = R.Colors.separator
+        return view
+    }()
+
+    func configure(with data: [WAChartsView.Data]) {
+
+        layoutIfNeeded()
+        addDashLines()
+    }
+}
+
+extension WAChartView {
+    override func setupViews() {
+        super.setupViews()
+
+        setupView(yAxisSeparator)
+        setupView(xAxisSeparator)
+    }
+
+    override func constraintViews() {
+        super.constraintViews()
+
+        NSLayoutConstraint.activate([
+            yAxisSeparator.leadingAnchor.constraint(equalTo: leadingAnchor),
+            yAxisSeparator.topAnchor.constraint(equalTo: topAnchor),
+            yAxisSeparator.bottomAnchor.constraint(equalTo: bottomAnchor),
+            yAxisSeparator.widthAnchor.constraint(equalToConstant: 1),
+            
+            xAxisSeparator.leadingAnchor.constraint(equalTo: leadingAnchor),
+            xAxisSeparator.trailingAnchor.constraint(equalTo: trailingAnchor),
+            xAxisSeparator.bottomAnchor.constraint(equalTo: bottomAnchor),
+            xAxisSeparator.heightAnchor.constraint(equalToConstant: 1)
+        ])
+    }
+
+    override func configureAppearance() {
+        super.configureAppearance()
+
+        backgroundColor = .clear
+    }
+}
+
+private extension WAChartView {
+    
+    func addDashLines(with counts: Int? = nil) {
+       
+        (0..<9).map { CGFloat($0) }.forEach {
+            addDashLine(at: bounds.height / 9 * $0)
+        }
+        
+        
+    }
+    
+    
+    func addDashLine(at yPosition: CGFloat) {
+        let startPoint = CGPoint(x: 0, y: yPosition)
+        let endPoint = CGPoint(x: bounds.width, y: yPosition)
+        
+        let dashPath = CGMutablePath()
+        dashPath.addLines(between: [startPoint, endPoint])
+        
+        let dashLayer = CAShapeLayer()
+        dashLayer.path = dashPath
+        dashLayer.strokeColor = R.Colors.separator.cgColor
+        dashLayer.lineWidth = 1
+        dashLayer.lineDashPattern = [6, 3]
+        
+        layer.addSublayer(dashLayer)
+    }
+}
--- WorkoutApp/WorkoutApp/Controllers/Base/Views/WAChartsView/WAChartsView.swift
+++ WorkoutApp/WorkoutApp/Controllers/Base/Views/WAChartsView/WAChartsView.swift
@@ -19,11 +19,12 @@ final class WAChartsView: WABaseView {
     private let yAxisView = YAxisView()
     private let xAxisView = XAxisView()
     
-    private let chartView = UIView() //WAChartView()
+    private let chartView = WAChartView()
     
     func configure(with data: [WAChartsView.Data]) {
         yAxisView.configure(with: data)
         xAxisView.configure(with: data)
+        chartView.configure(with: data)
     }
 }
 
@@ -44,14 +45,14 @@ extension WAChartsView {
             yAxisView.leadingAnchor.constraint(equalTo: leadingAnchor),
             yAxisView.bottomAnchor.constraint(equalTo: xAxisView.topAnchor, constant: -12),
             
-            xAxisView.leadingAnchor.constraint(equalTo: yAxisView.trailingAnchor, constant: 12),
+            xAxisView.leadingAnchor.constraint(equalTo: yAxisView.trailingAnchor, constant: 8),
             xAxisView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            xAxisView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            xAxisView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 4),
             
-            chartView.topAnchor.constraint(equalTo: topAnchor),
-            chartView.leadingAnchor.constraint(equalTo: yAxisView.trailingAnchor, constant: 12),
-            chartView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            chartView.bottomAnchor.constraint(equalTo: xAxisView.topAnchor, constant: -12)
+            chartView.topAnchor.constraint(equalTo: topAnchor, constant: 4),
+            chartView.leadingAnchor.constraint(equalTo: yAxisView.trailingAnchor, constant: 16),
+            chartView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -4),
+            chartView.bottomAnchor.constraint(equalTo: xAxisView.topAnchor, constant: -16)
         ])
     }
     
@@ -59,6 +60,5 @@ extension WAChartsView {
         super.configureAppearance()
         
         backgroundColor = .clear
-        chartView.backgroundColor = .cyan
     }
 }
